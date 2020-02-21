de875d169d578fe9cfbeba58ce1a53c80bbb0637
teemal <taylor.mallory@kdedirect.com>
2020-02-21 08:58:46 -0800

    finished var and mut chapter

---
+++ variables/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+    const NICE: u32 = 69;
+    println!("nice: {}", NICE);
+    let y = 6;
+    println!("y: {}", y);
+    let y = y + 1;
+    println!("new y: {}", y);
+}
