ff568f0ab4fb4363f4fa5c19fe8e270d7d9041fa
teemal <taylor.mallory@kdedirect.com>
2020-02-21 09:06:26 -0800

    finished variables chapter

---
+++ variables/target/debug/deps/variables-65c72bbf98fea8d3.d
@@ -0,0 +1,5 @@
+C:\Users\taylor\rust_book\variables\target\debug\deps\variables-65c72bbf98fea8d3.exe: src\main.rs
+
+C:\Users\taylor\rust_book\variables\target\debug\deps\variables-65c72bbf98fea8d3.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\taylor\rust_book\variables\target\debug\variables.exe: C:\Users\taylor\rust_book\variables\src\main.rs
