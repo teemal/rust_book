7b3bc4eeec8becfa616304b052ac3e3880ac142a
teemal <taylor.mallory@kdedirect.com>
2020-02-28 08:45:38 -0800

    .

---
+++ functions/target/debug/deps/functions-15992466cf5dd9de.d
@@ -0,0 +1,5 @@
+C:\Users\taylor\rust_book\functions\target\debug\deps\functions-15992466cf5dd9de.exe: src\main.rs
+
+C:\Users\taylor\rust_book\functions\target\debug\deps\functions-15992466cf5dd9de.d: src\main.rs
+
+src\main.rs:
---
+++ functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+C:\Users\taylor\rust_book\functions\target\debug\functions.exe: C:\Users\taylor\rust_book\functions\src\main.rs
